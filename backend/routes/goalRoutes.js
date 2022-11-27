1db20fd795b24b5a1a7db828195a15170229b8d9
omark01 <omark012000@gmail.com>
2022-11-27 01:04:07 -0500

    completed authorization using jwt and bycrypt

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const { protect } = require("../middleware/authMiddleware");
 const {
   getGoals,
   setGoal,
   updateGoal,
   deleteGoal,
 } = require("../controllers/goalController");
-router.route("/").get(getGoals).post(setGoal);
-router.route("/:id").put(updateGoal).delete(deleteGoal);
+router.route("/").get(protect, getGoals).post(protect, setGoal);
+router.route("/:id").put(protect, updateGoal).delete(protect, deleteGoal);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,9 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5000;
 const colors = require("colors");
 const connectDB = require("./config/db");
-connectDB()
 
+//Connect to the database
+connectDB();
 
 const app = express();
 //middleware
@@ -13,5 +14,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
 app.listen(port, () => console.log(`Server started on port ${port}`));
