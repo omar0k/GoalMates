1900a23ac61a4a0aa46bf1c690dd2fb501a129a8
omark01 <omark012000@gmail.com>
2022-11-28 00:51:39 -0500

    finished frontend and fully connected to backend, ready to deploy

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authRedcuer from "../features/auth/authSlice";
+import goalReducer from "../features/goals/goalSlice";
 export const store = configureStore({
   reducer: {
     auth: authRedcuer,
+    goals: goalReducer,
   },
 });
