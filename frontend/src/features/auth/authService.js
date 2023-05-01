26c63d7c414882d70908dbc1b37fffa5e453b0eb
omark01 <omark012000@gmail.com>
2023-04-30 20:11:07 -0400

    api fixes

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const API_URL = "/api/users/";
+const API_URL = "http://3.93.149.131:5000/api/users";
 
 // Register user
 const register = async (userData) => {
-  const response = await axios.post(API_URL, userData);
+  const response = await axios.post(`${API_URL}`, userData);
   // if (response.data) {
   //   localStorage.setItem("user", JSON.stringify(response.data));
   // }
--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "/api/goals/";
+const API_URL = "http://3.93.149.131:5000/api/goals/";
 
 // Create new goal
 const createGoal = async (goalData, token) => {
